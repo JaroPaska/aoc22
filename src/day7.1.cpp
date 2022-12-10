dad1808da403e05fe0b0e8ff6f783978c83a9fe7
Jaroslav P <jarko.paska@gmail.com>
2022-12-10 01:51:20 +0000

    day 7 rewrite

--- src/day7.1.cpp
+++ src/day7.1.cpp
@@ -1,6 +1,7 @@
-#include <algorithm>
 #include <iostream>
-#include <map>
+#include <numeric>
+#include <ranges>
+#include <span>
 #include <sstream>
 #include <string>
 #include <variant>
@@ -21,66 +22,91 @@ struct ListCmd {
     std::vector<Entry> entries;
 };
 
-struct ChangeDirCmd {
+struct CdCmd {
     std::string where;
 };
 
-using Cmd = std::variant<ListCmd, ChangeDirCmd>;
+using Cmd = std::variant<ListCmd, CdCmd>;
 using Path = std::vector<std::string>;
 
-constexpr auto update_path(const ChangeDirCmd& cd, Path& path) -> void {
-    if (cd.where == "/")
+struct Metadata {
+    std::vector<std::pair<Path, int>> own_sizes;
+    std::vector<std::pair<Path, std::vector<std::string>>> folders;
+    std::vector<std::pair<Path, int>> total_sizes;
+};
+
+template<class T1, class T2>
+constexpr auto find_or_insert(std::vector<std::pair<T1, T2>>& v, const T1& x) -> T2& {
+    auto it = std::ranges::find_if(v, [&](const std::pair<T1, T2>& p) { return p.first == x; });
+    if (it == std::ranges::end(v))
+        it = v.insert(v.end(), {x, T2()});
+    return it->second;
+}
+
+constexpr auto change_dir(Path path, std::string_view where) -> Path {
+    if (where == "/")
         path.clear();
-    else if (cd.where == "..")
+    else if (where == "..")
         path.pop_back();
     else
-        path.emplace_back(cd.where);
+        path.emplace_back(where);
+    return path;
 }
 
-constexpr auto get_own_sizes(const std::vector<Cmd>& cmds) -> std::vector<std::pair<Path, int>> {
-    Path path;
-    std::vector<std::pair<Path, int>> sizes;
-    for (const auto& cmd : cmds) {
-        if (std::holds_alternative<ListCmd>(cmd)) {
-            if (std::ranges::find_if(sizes, [&](const auto& p) { return p.first == path; }) != std::ranges::end(sizes))
-                continue;
-            const auto& ls = std::get<ListCmd>(cmd);
-            int sz = 0;
-            for (const auto& entry : ls.entries)
-                if (std::holds_alternative<File>(entry)) {
-                    const auto& file = std::get<File>(entry);
-                    sz += file.size;
-                }
-            sizes.push_back({path, sz});
-        } else {
-            const auto& cd = std::get<ChangeDirCmd>(cmd);
-            update_path(cd, path);
+constexpr auto process(Path path, std::span<const Cmd> cmds, Metadata&& metadata) -> Metadata {
+    if (cmds.empty())
+        return metadata;
+
+    const auto& cmd = cmds.front();
+    if (std::holds_alternative<ListCmd>(cmd)) {
+        const auto& list_cmd = std::get<ListCmd>(cmd);
+        auto& [own_sizes, folders, _] = metadata;
+        auto& own_size = find_or_insert(own_sizes, path);
+        own_size = 0;
+        auto& my_folders = find_or_insert(folders, path);
+        my_folders.clear();
+        for (const auto& entry : list_cmd.entries) {
+            if (std::holds_alternative<Dir>(entry)) {
+                const auto& dir = std::get<Dir>(entry);
+                my_folders.emplace_back(dir.name);
+            } else {
+                const auto& file = std::get<File>(entry);
+                own_size += file.size;
+            }
         }
+        metadata = process(path, cmds.subspan(1), std::move(metadata));
+    } else {
+        const auto& cd_cmd = std::get<CdCmd>(cmd);
+        metadata = process(change_dir(path, cd_cmd.where), cmds.subspan(1), std::move(metadata));
     }
-    return sizes;
+    auto& [own_sizes, folders, total_sizes] = metadata;
+    find_or_insert(total_sizes, path) = find_or_insert(own_sizes, path);
+    for (const auto& folder : find_or_insert(folders, path)) {
+        auto size = find_or_insert(total_sizes, change_dir(path, folder));
+        find_or_insert(total_sizes, path) += size;
+    }
+    return metadata;
 }
 
-constexpr auto get_children(const std::vector<Cmd>& cmds) -> std::vector<std::pair<Path, std::vector<std::string>>> {
-    Path path;
-    std::vector<std::pair<Path, std::vector<std::string>>> children;
-    for (const auto& cmd : cmds) {
-        if (std::holds_alternative<ListCmd>(cmd)) {
-            if (std::ranges::find_if(children, [&](const auto& p) { return p.first == path; }) != std::ranges::end(children))
-                continue;
-            const auto& ls = std::get<ListCmd>(cmd);
-            std::vector<std::string> names;
-            for (const auto& entry : ls.entries)
-                if (std::holds_alternative<Dir>(entry)) {
-                    const auto& dir = std::get<Dir>(entry);
-                    names.emplace_back(dir.name);
-                }
-            children.push_back({path, names});
-        } else {
-            const auto& cd = std::get<ChangeDirCmd>(cmd);
-            update_path(cd, path);
-        }
-    }
-    return children;
+static constexpr auto size_limit = 100000;
+
+constexpr auto small_dirs_size(const std::vector<Cmd>& cmds) -> int {
+    // clang-format off
+    auto metadata = process({}, std::span{cmds}, Metadata{});
+    auto small = metadata.total_sizes
+                           | std::views::transform([](const std::pair<Path, int>& p) { return p.second; })
+                           | std::views::filter([&](int size) { return size <= size_limit; });
+    return std::reduce(small.begin(), small.end());
+    // clang-format on
+}
+
+constexpr auto tests() -> void {
+    static_assert(
+        small_dirs_size({CdCmd{"/"}, ListCmd{{Dir{"a"}, File{14848514, "b.txt"}, File{8504156, "c.dat"}, Dir{"d"}}},
+                         CdCmd{"a"}, ListCmd{{Dir{"e"}, File{29116, "f"}, File{2557, "g"}, File{62596, "h.lst"}}},
+                         CdCmd{"e"}, ListCmd{{File{584, "i"}}}, CdCmd{".."}, CdCmd{".."}, CdCmd{"d"},
+                         ListCmd{{File{4060174, "j"}, File{8033020, "d.log"}, File{5626152, "d.ext"},
+                                  File{7214296, "k"}}}}) == 95437);
 }
 
 auto main() -> int {
@@ -98,7 +124,7 @@ auto main() -> int {
                 if (line == "$ ls")
                     cmds.emplace_back(ListCmd{});
                 else
-                    cmds.emplace_back(ChangeDirCmd{line.substr(5)});
+                    cmds.emplace_back(CdCmd{line.substr(5)});
             } else {
                 auto& ls = std::get<ListCmd>(cmds.back());
                 if (line.starts_with("dir"))
@@ -113,4 +139,5 @@ auto main() -> int {
             }
         }
     }
+    std::cout << small_dirs_size(cmds) << '\n';
 }
--- src/day7.2.cpp
+++ src/day7.2.cpp
@@ -1,32 +1,145 @@
 #include <algorithm>
 #include <iostream>
 #include <ranges>
+#include <span>
+#include <sstream>
 #include <string>
+#include <variant>
+#include <vector>
 
-constexpr auto is_marker(std::string s) -> bool {
-    std::ranges::sort(s);
-    return std::ranges::unique(s).begin() == s.end();
+struct Dir {
+    std::string name;
+};
+
+struct File {
+    int size;
+    std::string name;
+};
+
+using Entry = std::variant<Dir, File>;
+
+struct ListCmd {
+    std::vector<Entry> entries;
+};
+
+struct CdCmd {
+    std::string where;
+};
+
+using Cmd = std::variant<ListCmd, CdCmd>;
+using Path = std::vector<std::string>;
+
+struct Metadata {
+    std::vector<std::pair<Path, int>> own_sizes;
+    std::vector<std::pair<Path, std::vector<std::string>>> folders;
+    std::vector<std::pair<Path, int>> total_sizes;
+};
+
+template<class T1, class T2>
+constexpr auto find_or_insert(std::vector<std::pair<T1, T2>>& v, const T1& x) -> T2& {
+    auto it = std::ranges::find_if(v, [&](const std::pair<T1, T2>& p) { return p.first == x; });
+    if (it == std::ranges::end(v))
+        it = v.insert(v.end(), {x, T2()});
+    return it->second;
 }
 
-constexpr auto first_marker(std::string_view buffer, int marker_length) -> int {
+constexpr auto change_dir(Path path, std::string_view where) -> Path {
+    if (where == "/")
+        path.clear();
+    else if (where == "..")
+        path.pop_back();
+    else
+        path.emplace_back(where);
+    return path;
+}
+
+constexpr auto process(Path path, std::span<const Cmd> cmds, Metadata&& metadata) -> Metadata {
+    if (cmds.empty())
+        return metadata;
+
+    const auto& cmd = cmds.front();
+    if (std::holds_alternative<ListCmd>(cmd)) {
+        const auto& list_cmd = std::get<ListCmd>(cmd);
+        auto& [own_sizes, folders, _] = metadata;
+        auto& own_size = find_or_insert(own_sizes, path);
+        own_size = 0;
+        auto& my_folders = find_or_insert(folders, path);
+        my_folders.clear();
+        for (const auto& entry : list_cmd.entries) {
+            if (std::holds_alternative<Dir>(entry)) {
+                const auto& dir = std::get<Dir>(entry);
+                my_folders.emplace_back(dir.name);
+            } else {
+                const auto& file = std::get<File>(entry);
+                own_size += file.size;
+            }
+        }
+        metadata = process(path, cmds.subspan(1), std::move(metadata));
+    } else {
+        const auto& cd_cmd = std::get<CdCmd>(cmd);
+        metadata = process(change_dir(path, cd_cmd.where), cmds.subspan(1), std::move(metadata));
+    }
+    auto& [own_sizes, folders, total_sizes] = metadata;
+    find_or_insert(total_sizes, path) = find_or_insert(own_sizes, path);
+    for (const auto& folder : find_or_insert(folders, path)) {
+        auto size = find_or_insert(total_sizes, change_dir(path, folder));
+        find_or_insert(total_sizes, path) += size;
+    }
+    return metadata;
+}
+
+static constexpr auto total_space = 70000000;
+static constexpr auto need_unused = 30000000;
+
+constexpr auto smallest_big_dir(const std::vector<Cmd>& cmds) -> int {
     // clang-format off
-    return (std::views::iota(marker_length)
-          | std::views::transform([&](int i) { return std::make_pair(i, buffer.substr(i - marker_length, marker_length)); })
-          | std::views::filter([&](const std::pair<int, std::string_view>& p) { return is_marker(std::string(p.second)); })
-          | std::views::take(1)).front().first;
+    auto metadata = process({}, std::span{cmds}, Metadata{});
+    auto used = find_or_insert(metadata.total_sizes, {});
+    auto small = metadata.total_sizes
+                           | std::views::transform([](const std::pair<Path, int>& p) { return p.second; })
+                           | std::views::filter([&](int size) { return size >= need_unused - (total_space - used); });
+    return *std::min_element(small.begin(), small.end());
     // clang-format on
 }
 
-namespace tests {
-
-static_assert(!is_marker("bvwb"));
-static_assert(is_marker("vwbj"));
-static_assert(first_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5);
-
-} // namespace tests
+constexpr auto tests() -> void {
+    static_assert(
+        smallest_big_dir({CdCmd{"/"}, ListCmd{{Dir{"a"}, File{14848514, "b.txt"}, File{8504156, "c.dat"}, Dir{"d"}}},
+                          CdCmd{"a"}, ListCmd{{Dir{"e"}, File{29116, "f"}, File{2557, "g"}, File{62596, "h.lst"}}},
+                          CdCmd{"e"}, ListCmd{{File{584, "i"}}}, CdCmd{".."}, CdCmd{".."}, CdCmd{"d"},
+                          ListCmd{{File{4060174, "j"}, File{8033020, "d.log"}, File{5626152, "d.ext"},
+                                   File{7214296, "k"}}}}) == 24933642);
+}
 
 auto main() -> int {
-    std::string buffer;
-    std::getline(std::cin, buffer);
-    std::cout << first_marker(buffer, 4) << '\n';
+    std::vector<Cmd> cmds;
+    {
+        std::vector<std::string> lines;
+        {
+            std::string line;
+            while (std::getline(std::cin, line))
+                lines.emplace_back(line);
+        }
+
+        for (const auto& line : lines) {
+            if (line.starts_with("$")) {
+                if (line == "$ ls")
+                    cmds.emplace_back(ListCmd{});
+                else
+                    cmds.emplace_back(CdCmd{line.substr(5)});
+            } else {
+                auto& ls = std::get<ListCmd>(cmds.back());
+                if (line.starts_with("dir"))
+                    ls.entries.emplace_back(Dir{line.substr(4)});
+                else {
+                    std::stringstream ss(line);
+                    int size;
+                    std::string name;
+                    ss >> size >> name;
+                    ls.entries.emplace_back(File{size, name});
+                }
+            }
+        }
+    }
+    std::cout << smallest_big_dir(cmds) << '\n';
 }
