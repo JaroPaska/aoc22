89e722be1c89cbc0d4b8f69697e107601d9cf3c7
Jaroslav P <jarko.paska@gmail.com>
2022-12-13 19:23:08 +0100

    constexpr parse

--- src/day13.1.cpp
+++ src/day13.1.cpp
@@ -1,3 +1,5 @@
+#include <gsl>
+
 #include <array>
 #include <compare>
 #include <iostream>
@@ -7,30 +9,35 @@
 #include <variant>
 #include <vector>
 
+constexpr auto _isdigit(char c) -> bool {
+    return c >= '0' && c <= '9';
+}
+
+constexpr auto _stoi(std::string_view s) -> int {
+    int result = 0, sign = 1;
+    if (s.at(0) == '-')
+        sign = -1;
+    for (int i = 0; i < s.size(); i++) {
+        if (_isdigit(s.at(i)))
+            result = result * 10 + (s.at(i) - '0') * sign;
+    }
+    return result;
+}
+
 struct List {
     std::vector<std::variant<int, List>> elements;
 };
 
 constexpr auto operator<=>(const std::variant<int, List>& l, const std::variant<int, List>& r) -> std::strong_ordering;
 
-constexpr auto operator<=>(const List& l, const List& r) -> std::strong_ordering {
-    return l.elements <=> r.elements;
-}
+constexpr auto operator<=>(const List& l, const List& r) -> std::strong_ordering { return l.elements <=> r.elements; }
 
-constexpr auto operator<=>(int l, const List& r) -> std::strong_ordering {
-    return List{{l}} <=> r;
-}
+constexpr auto operator<=>(int l, const List& r) -> std::strong_ordering { return List{{l}} <=> r; }
 
-constexpr auto operator<=>(const List& l, int r) -> std::strong_ordering {
-    return l <=> List{{r}};
-}
+constexpr auto operator<=>(const List& l, int r) -> std::strong_ordering { return l <=> List{{r}}; }
 
 constexpr auto operator<=>(const std::variant<int, List>& l, const std::variant<int, List>& r) -> std::strong_ordering {
-    return std::visit([&](auto&& lv) {
-        return std::visit([&](auto&& rv) {
-            return lv <=> rv;
-        }, r);
-    }, l);
+    return std::visit([&](auto&& lv) { return std::visit([&](auto&& rv) { return lv <=> rv; }, r); }, l);
 }
 
 constexpr auto match_brackets(std::string_view line) -> std::vector<std::optional<int>> {
@@ -46,12 +53,12 @@ constexpr auto match_brackets(std::string_view line) -> std::vector<std::optiona
     return rb;
 }
 
-auto parse(std::string_view line, const std::vector<std::optional<int>>& rb, int l, int r) -> List {
+constexpr auto _parse(std::string_view line, const std::vector<std::optional<int>>& rb, int l, int r) -> List {
     List list;
     ++l;
     while (l < r) {
         if (rb.at(l)) {
-            list.elements.emplace_back(parse(line, rb, l, rb.at(l).value()));
+            list.elements.emplace_back(_parse(line, rb, l, rb.at(l).value()));
             l = rb.at(l).value() + 2;
         } else {
             int sz = 0;
@@ -64,6 +71,10 @@ auto parse(std::string_view line, const std::vector<std::optional<int>>& rb, int
     return list;
 }
 
+constexpr auto parse(std::string_view line) -> List {
+    return _parse(line, match_brackets(line), 0, gsl::narrow_cast<int>(line.size()));
+}
+
 constexpr auto sum_indices(const std::vector<std::array<List, 2>>& lists) {
     int s = 0;
     for (int i = 0; i < lists.size(); ++i)
@@ -73,6 +84,7 @@ constexpr auto sum_indices(const std::vector<std::array<List, 2>>& lists) {
 }
 
 constexpr auto tests() -> void {
+    static_assert(parse("[[1]]", ));
     static_assert(List{{1, 1, 3, 1, 1}} < List{{1, 1, 5, 1, 1}});
     static_assert(std::is_eq(List{{List{{1}}}} <=> List{{List{{1}}}}));
     static_assert(List{{List{{1}}, List{{2, 3, 4}}}} < List{{List{{1}}, 4}});
